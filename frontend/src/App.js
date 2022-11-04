b6348861f10dc573f0c5ad624a6a891516423857
Raymond Chen <rchen.utd@gmail.com>
2022-11-04 12:28:53 +1100

    add basic navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
+import NavigationBar from './components/NavigationBar';
 function App() {
   return (
     <div className="App">
+      <NavigationBar />
       <header className="App-header">React App</header>
     </div>
   );
---
+++ frontend/src/components/NavigationBar.js
@@ -0,0 +1,18 @@
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+
+export default function NavigationBar() {
+  return (
+    <Navbar bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="/">Quickpoll</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="#majority">Majority</Nav.Link>
+          <Nav.Link href="#proportional">Proportional</Nav.Link>
+          <Nav.Link href="#prefrential">Preferential</Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+}
