bd9a3b151c1e233082114086f7b735f0bf85eb4f
Raymond Chen <rchen.utd@gmail.com>
2022-11-04 14:11:19 +1100

    testing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import NavigationBar from './components/NavigationBar';
 function App() {
+  var end = window.location.href;
+  console.log(end);
   return (
     <div className="App">
       <NavigationBar />
