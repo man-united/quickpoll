ab48155097f9e148ab0834ca33513981df9aa3bc
Raymond Chen <rchen.utd@gmail.com>
2023-09-30 17:07:46 +1000

    initial project setup

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <header>Hello World</header>
+    </div>
+  );
+}
+
+ssf;
+
+export default App;
